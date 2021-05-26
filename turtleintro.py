3d035994942c848b7cf2fe8255296c7a14ec5e56
Brian Thai <brian.s.thai@gmail.com>
2021-05-25 23:16:52 -0700

    added sides affecting drawing

--- turtleintro.py
+++ turtleintro.py
@@ -2,8 +2,10 @@ import turtle
 t = turtle.Pen()
 turtle.bgcolor("black")
 turtle.speed(10)
+sides = 5
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
-for x in range(10,100):
+for x in range(360):
     t.pencolor(colors[x%len(colors)])
-    t.circle(x)
-    t.left(360/len(colors))
+    t.forward(x * 3/sides + x)
+    t.left(360/sides + 1)
+    t.width(x*sides/200)
