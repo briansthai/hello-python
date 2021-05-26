4e240662b17c34900810e090fdf9415b65d58fab
Brian Thai <brian.s.thai@gmail.com>
2021-05-25 23:50:11 -0700

    writing text with turtle

--- turtleintro.py
+++ turtleintro.py
@@ -3,6 +3,7 @@ import turtle
 sides = eval(turtle.textinput("Sides", "Enter the number of sides"))
 print(sides)
 
+screen = turtle.Screen()
 t = turtle.Pen()
 turtle.bgcolor("black")
 turtle.speed(10)
@@ -10,6 +11,8 @@ turtle.speed(10)
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 for x in range(360):
     t.pencolor(colors[x%len(colors)])
+    t.penup()
     t.forward(x * 3/sides + x)
+    t.pendown()
+    t.write("its me brian!", font = ("Arial", int((x+4)/4), "bold"))
     t.left(360/sides + 1)
-    t.width(x*sides/200)
