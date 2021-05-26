68fc833b85c31ca844ca0147f7073cbac078a686
Brian Thai <brian.s.thai@gmail.com>
2021-05-25 23:06:40 -0700

    added bgcolor, turtle speed

--- turtleintro.py
+++ turtleintro.py
@@ -1,5 +1,7 @@
 import turtle
 t = turtle.Pen()
+turtle.bgcolor("black")
+turtle.speed(10)
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 for x in range(10,100):
     t.pencolor(colors[x%len(colors)])
