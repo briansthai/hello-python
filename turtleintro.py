ded92f67bfa6d240915a75ebfa047681e3dd9bfb
Brian Thai <brian.s.thai@gmail.com>
2021-05-25 23:18:41 -0700

    rainbow spiral, a favorite by me!

--- turtleintro.py
+++ turtleintro.py
@@ -2,7 +2,7 @@ import turtle
 t = turtle.Pen()
 turtle.bgcolor("black")
 turtle.speed(10)
-sides = 5
+sides = 6
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 for x in range(360):
     t.pencolor(colors[x%len(colors)])
