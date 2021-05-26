a6285ec5cc7b089f40b597af72b77ca86f6b84a3
Brian Thai <brian.s.thai@gmail.com>
2021-05-25 23:03:38 -0700

    implemeted multi color; rainbow colored flower

--- turtleintro.py
+++ turtleintro.py
@@ -1,6 +1,7 @@
 import turtle
 t = turtle.Pen()
-t.pencolor("blue")
-for x in range(1,600,10):
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+for x in range(10,100):
+    t.pencolor(colors[x%len(colors)])
     t.circle(x)
-    t.left(91)
+    t.left(360/len(colors))
